7835b9c601738b86c74b38485b989b6708440c5c
golluroSICKAG <45095769+golluroSICKAG@users.noreply.github.com>
2026-07-07 08:36:07 +0200

    Release 2.1.0 (#8)
    # Release 2.1.0
    
    ## New features
    - Added "delay"-block to forward incoming data after a specific time
    
    ## Improvement
    - Extended SUBTRACT logic
    
    ## Bugfixes
    - Error in "SUBTRACT", "AND_PREV" and "OR_PREV" logic

--- CSK_Module_FlowConfig/scripts/CSK_FlowConfig_TimerProcessing.lua
+++ CSK_Module_FlowConfig/scripts/CSK_FlowConfig_TimerProcessing.lua
@@ -5,10 +5,53 @@ local timer = {}
 local tmrFunctions = {}
 local toggleStatus = {}
 
+local tmrDelays = {}
+local forwardFunctions = {}
+local saveDataFunctions = {}
+local tempForwardData = {}
+
 -- Timer to wait 1 second
 local tmrStartTimers = Timer.create()
 tmrStartTimers:setExpirationTime(1000)
 tmrStartTimers:setPeriodic(false)
+
+local function addDelayBlock(sourceEvent, delay)
+  local functionName = tostring(sourceEvent) .. '_Delay' .. tostring(delay)
+
+  if not tmrDelays[functionName] then
+    local tempDelayTmr = Timer.create()
+    tempDelayTmr:setPeriodic(false)
+    tempDelayTmr:setExpirationTime(delay)
+    tmrDelays[functionName] = tempDelayTmr
+
+    local isServed = Script.isServedAsEvent("CSK_FlowConfig." .. functionName)
+    if not isServed then
+      Script.serveEvent("CSK_FlowConfig." .. functionName, functionName, 'auto')
+    end
+
+    -- Function to save data to forward
+    local function tempSaveData(data)
+      if data ~= nil then
+        tempForwardData[functionName] = data
+      end
+      tmrDelays[functionName]:start()
+    end
+    saveDataFunctions[functionName] = tempSaveData
+    Script.register(sourceEvent, saveDataFunctions[functionName])
+
+    -- Function to forward event with delay
+    local function forwardData()
+      if tempForwardData[functionName] ~= nil then
+        Script.notifyEvent(functionName, tempForwardData[functionName])
+      else
+        Script.notifyEvent(functionName)
+      end
+    end
+    forwardFunctions[functionName] = forwardData
+    Timer.register(tmrDelays[functionName], 'OnExpired', forwardFunctions[functionName])
+  end
+end
+Script.serveFunction('CSK_FlowConfig.addDelayBlock', addDelayBlock)
 
 local function addTimer(value, valueType, cycleTime)
   local fullString = tostring(valueType) .. '_' .. tostring(cycleTime) .. '_' .. tostring(value)
@@ -79,9 +122,21 @@ local function reset()
   for toggleKey, _ in pairs(toggleStatus) do
     toggleStatus[toggleKey] = nil
   end
+  for key, _ in pairs(tmrDelays) do
+    tmrDelays[key] = nil
+    forwardFunctions[key] = nil
+    saveDataFunctions[key] = nil
+    tempForwardData[key] = nil
+  end
 
   timer = {}
   toggleStatus = {}
+
+  tmrDelays = {}
+  forwardFunctions = {}
+  saveDataFunctions = {}
+  tempForwardData = {}
+
 end
 Script.register('CSK_FlowConfig.OnClearOldFlow', reset)
 Script.register('CSK_FlowConfig.OnStopFlowConfigProviders', reset)
